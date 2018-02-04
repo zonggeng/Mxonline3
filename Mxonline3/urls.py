0f67f3a1528f6919faeac4362a75bae14a5a0b2e
SillyGG <137100856@163.com>
2018-02-04 12:18:19 +0800

    Xadmin的安装与源码安装，配置setting中extra_apps

--- Mxonline3/urls.py
+++ Mxonline3/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.urls import path, include, re_path
+# from django.contrib import admin
+import xadmin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    # Django 2.0.1中不需要加r也不需要加^
+    path('xadmin/', xadmin.site.urls),
 ]
