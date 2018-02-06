f34d194e77e7354ea707eae03e28f119a25b3b5d
SillyGG <137100856@163.com>
2018-02-06 21:10:10 +0800

    将机构详情展示完毕，为课程机构添加了收藏功能。修复了index未登录状态下爆炸的错误

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -4,12 +4,16 @@ from django.db import models
 
 
 # 课程信息表
+from organization.models import CourseOrg
+
+
 class Course(models.Model):
     DEGREE_CHOICES = (
         ("cj", u"初级"),
         ("zj", u"中级"),
         ("gj", u"高级")
     )
+    course_org = models.ForeignKey(CourseOrg, on_delete=models.CASCADE, verbose_name=u"所属机构", null=True, blank=True)
     name = models.CharField(max_length=50, verbose_name=u"课程名")
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
     # TextField允许我们不输入长度。可以输入到无限大。暂时定义为TextFiled，之后更新为富文本
@@ -27,6 +31,7 @@ class Course(models.Model):
     # 保存点击量，点进页面就算
     click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
 
     class Meta:
         verbose_name = u"课程"
